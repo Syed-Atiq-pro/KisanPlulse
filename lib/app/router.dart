ab93d72ef47c581a6ce30a8a72d19bcb9173dd9a
Atiq Syed <syedatiq4953@gmail.com>
2026-09-08 21:49:28 +0530

    feat: add application routing

---
+++ lib/app/router.dart
@@ -0,0 +1,18 @@
+import 'package:go_router/go_router.dart';
+
+import '../features/auth/presentation/login_page.dart';
+import '../features/dashboard/presentation/dashboard_page.dart';
+
+final appRouter = GoRouter(
+  initialLocation: '/login',
+  routes: [
+    GoRoute(
+      path: '/login',
+      builder: (context, state) => const LoginPage(),
+    ),
+    GoRoute(
+      path: '/dashboard',
+      builder: (context, state) => const DashboardPage(),
+    ),
+  ],
+);
